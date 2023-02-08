9e60c2a4457a147d056813b9dc0cb35f334656c9
TBSDrJ <marcus.jaiclin@bishops.com>
2023-02-08 12:32:10 -0800

    Add cache/prefetch to checkpoints model

--- faces_checkpoints.py
+++ faces_checkpoints.py
@@ -6,6 +6,7 @@ import tensorflow.keras.layers as layers
 import tensorflow.keras.losses as losses
 import tensorflow.keras.optimizers as optimizers
 import tensorflow.keras.callbacks as callbacks
+import tensorflow.data as data
 
 train = utils.image_dataset_from_directory(
     'images',
@@ -29,6 +30,9 @@ test = utils.image_dataset_from_directory(
 
 print("Class Names:")
 pprint(train.class_names)
+
+train = train.cache().prefetch(buffer_size = data.AUTOTUNE)
+test = test.cache().prefetch(buffer_size = data.AUTOTUNE)
 
 class Net():
     def __init__(self, input_shape):
@@ -102,14 +106,14 @@ callbacks = [
 net.model.fit(
     train,
     batch_size = 32,
-    epochs = 20,
+    epochs = 40,
     verbose = 2,
     validation_data = test,
     validation_batch_size = 32,
     callbacks = callbacks,
 )
 
-save_path = 'saves/faces_model_save_2023_02_05__20_epochs'
+save_path = 'saves/faces_model_save_2023_02_08__40_epochs'
 net.model.save(save_path)
 with open(f'{save_path}/class_names.data', 'wb') as f:
     pickle.dump(train.class_names, f)
